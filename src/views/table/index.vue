a9bcc50c64e96d0b2d124820102ab1d4104aae81
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-10-31 10:10:12 -0700

    Test loading some AP Reports

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -8,35 +8,49 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column align="center" label="Report ID" width="95">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          {{ scope.row.id }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="System">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          {{ scope.row.systemName }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Body">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          {{ scope.row.bodyName }}
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="CMDR" width="200" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          <span>{{ scope.row.cmdrName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Latitude" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.latitude }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Longitude" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.longitude }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Type" width="200" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.type }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Client Version" width="200" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.clientVersion }}
         </template>
       </el-table-column>
       <el-table-column class-name="status-col" label="Status" width="110" align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <el-tag :type="scope.row.reportStatus | statusFilter">{{ scope.row.reportStatus }}</el-tag>
         </template>
       </el-table-column>
     </el-table>
@@ -50,8 +64,8 @@ export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
+        accepted: 'success',
+        duplicate: 'gray',
         deleted: 'danger'
       }
       return statusMap[status]
@@ -70,7 +84,8 @@ export default {
     fetchData() {
       this.listLoading = true
       getList().then(response => {
-        this.list = response.data.items
+        this.list = response
+        console.log(response)
         this.listLoading = false
       })
     }
