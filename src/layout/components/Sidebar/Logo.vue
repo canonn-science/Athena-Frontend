76484fa10db69f6b2494618826892e02f9e33eef
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-10-31 13:47:12 -0700

    Update title and logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: 'Canonn Science',
+      logo: 'https://api.canonn.tech/uploads/40bfc7d870e54925ad0f769e7b0b1f9a.png'
     }
   }
 }
